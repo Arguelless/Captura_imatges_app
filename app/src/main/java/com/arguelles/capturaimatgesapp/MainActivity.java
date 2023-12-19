c2aafa8324ad11db8389858fc57fa9da323277f1
Arguelless <marguelles@protonmail.com>
2023-12-19 15:46:03 +0100

    asdf

--- app/src/main/java/com/arguelles/capturaimatgesapp/MainActivity.java
+++ app/src/main/java/com/arguelles/capturaimatgesapp/MainActivity.java
@@ -3,26 +3,50 @@ import androidx.activity.result.ActivityResult;
 import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
-import android.widget.ImageView;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
-    private ActivityResultLauncher<Intent> someActivityResultLauncher;
+
+    private static final int REQUEST_CAMERA_PERMISSION = 1;
+
+    private ImageView imageView;
+    private ActivityResultLauncher<Intent> cameraActivityResultLauncher;
+    private ActivityResultLauncher<Intent> galleryActivityResultLauncher;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        someActivityResultLauncher = registerForActivityResult(
+        imageView = findViewById(R.id.imgView);
+
+        cameraActivityResultLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        handleCameraResult(result.getData());
+                    }
+                });
+
+        galleryActivityResultLauncher = registerForActivityResult(
                 new ActivityResultContracts.StartActivityForResult(),
                 new ActivityResultCallback<ActivityResult>() {
                     @Override
@@ -38,16 +62,48 @@ public class MainActivity extends AppCompatActivity {
                     }
                 });
 
-        // Obtener referencia al botón
         Button btnOpenGallery = findViewById(R.id.btnOpenGallery);
 
-        // Establecer OnClickListener para el botón
         btnOpenGallery.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 openGallery();
             }
         });
+
+        Button cameraButton = findViewById(R.id.cameraButton);
+        cameraButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openCamera();
+            }
+        });
+    }
+
+    private void openCamera() {
+        if (checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            cameraActivityResultLauncher.launch(intent);
+        } else {
+            requestPermissions(new String[]{Manifest.permission.CAMERA}, REQUEST_CAMERA_PERMISSION);
+        }
+    }
+
+    private void handleCameraResult(Intent data) {
+        if (data != null) {
+            Bundle extras = data.getExtras();
+            if (extras != null) {
+                Bitmap imageBitmap = (Bitmap) extras.get("data");
+                if (imageBitmap != null) {
+                    // Configurar la imagen en el ImageView
+                    imageView.setImageBitmap(imageBitmap);
+                } else {
+                    Toast.makeText(this, "Error al obtener la imagen", Toast.LENGTH_SHORT).show();
+                }
+            } else {
+                Toast.makeText(this, "Error al obtener la imagen", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 
     private void openGallery() {
@@ -56,7 +112,6 @@ public class MainActivity extends AppCompatActivity {
         intent.setType("image/jpg");
         intent.putExtra(Intent.EXTRA_LOCAL_ONLY, true);
         //Launch activity to get result
-        someActivityResultLauncher.launch(intent);
+        galleryActivityResultLauncher.launch(intent);
     }
 }
-
